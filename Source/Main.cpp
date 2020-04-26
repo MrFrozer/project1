ec1f9ac4f78e42daaeb141d63a37ab5d1b576c89
yurkaxpeh <64321288+yurkaxpeh@users.noreply.github.com>
2020-04-26 13:37:34 +0300

    .

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,9 +1,5 @@
-#include <SDL.h>
 
 int main(int argc, char* argv[])
 {
-	SDL_Window* window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_OPENGL);
-
-	SDL_DestroyWindow(window);
 	return 0;
 }
