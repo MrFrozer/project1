0f45ec30b12dbe692a4900a22c082caf8c45a14e
yurkaxpeh <64321288+yurkaxpeh@users.noreply.github.com>
2020-04-26 13:31:46 +0300

    .

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,6 +2,8 @@
 
 int main(int argc, char* argv[])
 {
-	SDL_Window* s;
+	SDL_Window* window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_OPENGL);
+
+	SDL_DestroyWindow(window);
 	return 0;
 }
